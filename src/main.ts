f367917fd3a0ae853d3f7712c65f9ad04f995f4e
gabrielcdiniz <gabriel.dinizcd1997@gmail.com>
2024-04-24 00:34:33 -0300

    feat: add swagger settings

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,43 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configServer = app.get<ConfigService>(ConfigService);
+  const serverPort = configServer.getOrThrow('SERVER_PORT');
+
+  const TITLE = 'Polaris';
+  const DESCRIPTION = 'The main API of "Polaris"';
+  const API_VERSION = '1.0';
+
+  app.enableCors();
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle(TITLE)
+    .setDescription(DESCRIPTION)
+    .setVersion(API_VERSION)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config, {
+    ignoreGlobalPrefix: false,
+  });
+
+  SwaggerModule.setup('docs', app, document, {
+    customSiteTitle: TITLE,
+    useGlobalPrefix: false,
+  });
+
+  await app.listen(serverPort);
+
+  const url = await app.getUrl();
+  logger.log(`listening app at ${url}`);
 }
 bootstrap();
