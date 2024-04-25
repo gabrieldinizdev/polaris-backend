d48dfcfc01616b3b2916a3b3ead2181b76e31e86
gabrielcdiniz <gabriel.dinizcd1997@gmail.com>
2024-04-24 22:40:47 -0300

    Revert "Revert "[ FEATURE ] Initial Settings (#2)""
    This reverts commit cc8ffffd15f0a338fad873a3de82bf39c843e56d.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      expandVariables: true,
+      cache: true,
+      envFilePath: '.env',
+    }),
+
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const uri = configService.getOrThrow('DB_URI');
+
+        return { uri };
+      },
+    }),
+
+    ProductsModule,
+  ],
 })
 export class AppModule {}
