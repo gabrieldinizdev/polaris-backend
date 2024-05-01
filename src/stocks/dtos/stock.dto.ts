7bb18eb964d9a9746b4d0ffa0afb85bcdf934de6
gabrielcdiniz <gabriel.dinizcd1997@gmail.com>
2024-05-01 10:31:10 -0300

    refactor(code-review): fix code review changes

--- src/stocks/dtos/stock.dto.ts
+++ src/stocks/dtos/stock.dto.ts
@@ -1,9 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-import { IsArray, IsMongoId, IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 import { CommonFieldsDTO } from '@/shared/dtos/common';
-import { StockItemDTO } from '@/stock-items/dtos';
 
 export class StockDTO extends CommonFieldsDTO {
   @ApiProperty({
@@ -13,13 +12,4 @@ export class StockDTO extends CommonFieldsDTO {
   @IsString()
   @IsNotEmpty()
   public readonly name: string;
-
-  @ApiProperty({
-    description: 'Stock items',
-    type: StockItemDTO,
-    isArray: true,
-  })
-  @IsArray()
-  @IsMongoId({ each: true })
-  public readonly items: StockItemDTO[];
 }
